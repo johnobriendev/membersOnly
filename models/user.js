bc95c66a64e23657d743f01eb7ec85b80945aa98
John O'Brien <johnobrien.dev@gmail.com>
2024-06-06 15:38:44 -0400

    update user schema

--- models/user.js
+++ models/user.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
+  first_name: { type: String, required: true, maxLength: 50, minLength: 3 },
+  last_name: { type: String, required: true, maxLength: 50, minLength: 3 },
+  email: { type: String, required: true },
   password: { type: String, required: true },
-  membershipStatus: { type: Boolean, default: false },
+  isMember: { type: Boolean, default: false },
   isAdmin: { type: Boolean, default: false }
+});
+
+UserSchema.virtual('fullname').get(function () {
+  return `${this.first_name} ${this.last_name}`;
 });
 
 module.exports = mongoose.model('User', UserSchema);
