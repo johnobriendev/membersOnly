79b1876d8f31a1bff75a3c4e41d4aea9da8db0d5
John O'Brien <johnobrien.dev@gmail.com>
2024-06-08 22:00:09 -0400

    create routes for join in users

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
+const isAuthenticated = require('../middleware/isAuthenticated');
+
+// Display join form
+router.get('/join', isAuthenticated, userController.join_get);
+
+// Handle join form submission
+router.post('/join', isAuthenticated, userController.join_post);
 
 module.exports = router;
