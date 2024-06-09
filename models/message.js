a019a446121ac9c3212be2149cd2f2728d63de81
John O'Brien <johnobrien.dev@gmail.com>
2024-06-08 22:47:20 -0400

    modify message dates with luxon

--- models/message.js
+++ models/message.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
   title: { type: String, required: true },
@@ -8,4 +9,7 @@ const MessageSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
+MessageSchema.virtual('formattedTimestamp').get(function() {
+  return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_MED);
+});
 module.exports = mongoose.model('Message', MessageSchema);
